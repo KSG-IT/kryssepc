c72575bdf85cfc914c3bdfa8e21f5ea4facc0dfa
Magnus Buvarp <magnus.buvarp@wartsila.com>
2018-09-19 18:55:47 +0200

    Initialized Vue and Webpack, standard setup ESLint and stuff

---
+++ kryssepc/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+*, ::before, ::after {
+    box-sizing: border-box;
+}
+html, body {
+    padding: 0;
+    margin: 0;
+}
+</style>
